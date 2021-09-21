7a490eda31dcedea13bd72a6f7a0ddb5899f420f
guillermorangel <guillermo.m.rangel@gmail.com>
2021-09-21 13:27:34 -0400

    Scope the styles of the app component

--- build-a-bot/src/App.vue
+++ build-a-bot/src/App.vue
@@ -29,14 +29,17 @@ export default {
 </script>
 
 <style>
+  body {
+    background: linear-gradient(to bottom, #555, #999);
+    background-attachment: fixed;
+  }
+</style>
+
+<style scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-}
-body {
-  background: linear-gradient(to bottom, #555, #999);
-  background-attachment: fixed;
 }
 main {
   margin: 0 auto;
