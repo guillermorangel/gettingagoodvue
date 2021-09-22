d74134bdb2fd77693db56076412a66dfb897cbde
guillermorangel <guillermo.m.rangel@gmail.com>
2021-09-22 15:26:53 -0400

    Use slot to inject wrapped content into component

--- build-a-bot/src/components/shared/CollapsibleSection.vue
+++ build-a-bot/src/components/shared/CollapsibleSection.vue
@@ -4,6 +4,8 @@
       <span v-if="open" @click="open = !open" >&#x25B2; Collapse</span>
       <span v-if="!open" @click="open = !open">&#x25BC; Expand</span>
     </div>
+    <slot v-if="open">
+    </slot>
   </div>
 </template>
 
