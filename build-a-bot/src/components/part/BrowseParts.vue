51d3e4d9ec18d9cc7b497d4897e579645aba0a29
guillermorangel <guillermo.m.rangel@gmail.com>
2021-09-24 10:34:29 -0400

    Add content for the browse parts pages

---
+++ build-a-bot/src/components/part/BrowseParts.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h1>Browse Parts</h1>
+    <ul class="menu">
+      <li>
+        <span>Heads</span>
+      </li>
+      <li>
+        <span>Arms</span>
+      </li>
+      <li>
+        <span>Torsos</span>
+      </li>
+      <li>
+        <span>Bases</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BrowseParts',
+};
+</script>
+
+<style scoped>
+  h1 {
+    text-align: center;
+  }
+  .menu {
+    display: flex;
+    justify-content: space-around;
+    margin: 0 100px;
+    font-size: 20px;
+  }
+  ul {
+    list-style-type: none;
+  }
+  a {
+    color: palevioletred;
+  }
+</style>
