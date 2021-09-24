bc0a1e75ee8f3cbf857ba77a81af354c05e90203
guillermorangel <guillermo.m.rangel@gmail.com>
2021-09-24 19:17:50 -0400

    Define child routes for the parent route

--- build-a-bot/src/router/index.js
+++ build-a-bot/src/router/index.js
@@ -1,6 +1,11 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 import HomePage from '../components/HomePage.vue';
 import RobotBuilder from '../components/robot/RobotBuilder.vue';
+import BrowseParts from '../components/part/BrowseParts.vue';
+import RobotHeads from '../components/part/RobotHeads.vue';
+import RobotArms from '../components/part/RobotArms.vue';
+import RobotTorsos from '../components/part/RobotTorsos.vue';
+import RobotBases from '../components/part/RobotBases.vue';
 import PartInfo from '../components/part/PartInfo.vue';
 import Search from '../components/search/Search.vue';
 
@@ -14,6 +19,30 @@ export default createRouter({
     path: '/build',
     name: 'Build',
     component: RobotBuilder,
+  }, {
+    path: '/parts/browse',
+    name: 'BrowseParts',
+    component: BrowseParts,
+    children: [
+      {
+        name: 'BrowseHeads',
+        path: 'heads',
+        component: RobotHeads,
+      },
+      {
+        name: 'BrowseArms',
+        path: 'arms',
+        component: RobotArms,
+      }, {
+        name: 'BrowseTorsos',
+        path: 'toros',
+        component: RobotTorsos,
+      }, {
+        name: 'BrowseBases',
+        path: 'bases',
+        component: RobotBases,
+      },
+    ],
   }, {
     path: '/parts/:partType/:partId',
     name: 'Parts',
