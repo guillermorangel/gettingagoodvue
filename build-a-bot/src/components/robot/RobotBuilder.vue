626a6ed9b49f660086c9800ccf118627fb8c843b
guillermorangel <guillermo.m.rangel@gmail.com>
2021-09-26 14:21:40 -0400

    Use store retain cart information, create and commit mutation

--- build-a-bot/src/components/robot/RobotBuilder.vue
+++ build-a-bot/src/components/robot/RobotBuilder.vue
@@ -96,7 +96,7 @@ export default {
         + robot.rightArm.cost
         + robot.base.cost;
 
-      this.cart.push({ ...robot, cost });
+      this.$store.commit('addRobotToCart', { ...robot, cost });
       this.addedToCart = true;
     },
   },
