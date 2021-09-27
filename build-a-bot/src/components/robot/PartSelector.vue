6aa49683a698332e54114987c1c79fc630dbe454
guillermorangel <guillermo.m.rangel@gmail.com>
2021-09-27 11:06:41 -0400

    Use args and modifiers to pass data into directive

--- build-a-bot/src/components/robot/PartSelector.vue
+++ build-a-bot/src/components/robot/PartSelector.vue
@@ -5,7 +5,7 @@
     </router-link>
     <button @click="selectPreviousPart()" class="prev-selector"></button>
     <button @click="selectNextPart()" class="next-selector"></button>
-    <span v-pin class="sale" v-show="selectedPart.onSale">Sale!</span>
+    <span v-pin:position.top.right class="sale" v-show="selectedPart.onSale">Sale!</span>
   </div>
 </template>
 
--- build-a-bot/src/components/shared/directive/pin-directive.js
+++ build-a-bot/src/components/shared/directive/pin-directive.js
@@ -1,8 +1,14 @@
 export default {
-  beforeMount(element) {
+  beforeMount(element, binding) {
+    // console.log('arg:', binding.arg);
+    // console.log('modifiers:', binding.modifiers);
+    if (binding.arg !== 'position') return;
+
     const el = element;
+    Object.keys(binding.modifiers).forEach((key) => {
+      el.style[key] = '5px';
+    });
+
     el.style.position = 'absolute';
-    el.style.bottom = '5px';
-    el.style.right = '5px';
   },
 };
