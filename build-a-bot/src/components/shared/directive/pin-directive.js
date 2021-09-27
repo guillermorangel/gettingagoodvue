553b4d8806ab50b145f139d10e55822c0b2cdf4d
guillermorangel <guillermo.m.rangel@gmail.com>
2021-09-27 11:52:20 -0400

    Use the directive's updated life cycle hook to show changes when updated

--- build-a-bot/src/components/shared/directive/pin-directive.js
+++ build-a-bot/src/components/shared/directive/pin-directive.js
@@ -1,11 +1,17 @@
+function applyStyle(element, binding) {
+  const el = element;
+  Object.keys(binding.value).forEach((key) => {
+    el.style[key] = binding.value[key];
+  });
+  el.style.position = 'absolute';
+}
+
 export default {
   beforeMount(element, binding) {
-    const el = element;
+    applyStyle(element, binding);
+  },
 
-    Object.keys(binding.value).forEach((key) => {
-      el.style[key] = binding.value[key];
-    });
-
-    el.style.position = 'absolute';
+  updated(element, binding) {
+    applyStyle(element, binding);
   },
 };
