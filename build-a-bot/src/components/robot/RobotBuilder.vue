72920006c362c0c593a42e2892d3203d12fc4596
guillermorangel <guillermo.m.rangel@gmail.com>
2021-09-27 01:16:52 -0400

    Namespace the robots module to keep names separated, specify the module for actions
    State is always namespaced by default. Mutations, actions, and getters are not namespaced by default

--- build-a-bot/src/components/robot/RobotBuilder.vue
+++ build-a-bot/src/components/robot/RobotBuilder.vue
@@ -68,7 +68,7 @@ export default {
     }
   },
   created() {
-    this.$store.dispatch('getParts');
+    this.$store.dispatch('robots/getParts');
   },
   data() {
     return {
@@ -100,7 +100,7 @@ export default {
         + robot.rightArm.cost
         + robot.base.cost;
 
-      this.$store.dispatch('addRobotToCart', { ...robot, cost })
+      this.$store.dispatch('robots/addRobotToCart', { ...robot, cost })
         .then(() => this.$router.push('/cart'));
 
       this.addedToCart = true;
